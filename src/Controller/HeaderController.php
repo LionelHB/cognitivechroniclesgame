8e85f8b7850be1a2eaf1f8d7f801d528b0cabb99
LionelHB <shinoushin@gmail.com>
2023-08-09 06:31:59 +0200

    fix (fixture & category)

--- src/Controller/HeaderController.php
+++ src/Controller/HeaderController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\SubCategory;
+use App\Repository\CategoryRepository;
+use App\Repository\SubCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HeaderController extends AbstractController
 {
+
+
+    public function __construct(
+        private CategoryRepository $CategoryRepository,
+    ) {
+    }
+
     #[Route('/category', name: 'app_category')]
-    public function category(): Response
+    public function category(CategoryRepository $categoryRepository, SubCategoryRepository $subCategoryRepository): Response
     {
+
+        $subCategory1 = $categoryRepository->find(1);
+        $subCategories = $subCategory1->getSubCategories();
+        $subCategory2 = $categoryRepository->find(2);
+        $subCategories = $subCategory2->getSubCategories();
+        $subCategory3 = $categoryRepository->find(3);
+        $subCategories = $subCategory3->getSubCategories();
+
+        $category1 = $categoryRepository->find(1);
+        $category2 = $categoryRepository->find(2);
+        $category3 = $categoryRepository->find(3);
+        
+
         return $this->render('front/pages/category.html.twig', [
             'controller_name' => 'HeaderController',
+            'category1' => $category1,
+            'category2' => $category2,
+            'category3' => $category3,
+            'subCategories' => $subCategories,
+            'subCategory1' => $subCategory1,
+            'subCategory2' => $subCategory2,
+            'subCategory3' => $subCategory3,
+
         ]);
     }
 
@@ -23,6 +55,4 @@ class HeaderController extends AbstractController
             'controller_name' => 'HeaderController',
         ]);
     }
-
-
 }
