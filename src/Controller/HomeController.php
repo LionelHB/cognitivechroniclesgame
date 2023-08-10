7b2c76cde481c119a574e3590493db196812cde5
LionelHB <shinoushin@gmail.com>
2023-08-10 22:39:26 +0200

    fix (fixture + screen nft)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\NftRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private NftRepository $nftRepository,
+    ) {
+    }
+
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(NftRepository $nftRepository): Response
     {
+        $nfts = $nftRepository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'nfts' => $nfts,
         ]);
     }
 }
